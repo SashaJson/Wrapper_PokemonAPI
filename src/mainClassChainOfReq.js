705b5be55db4a0d2c0f9ad663b501102408d11fb
SashaJson <alexander.shulga@teradek.com>
2020-06-07 02:08:39 +0300

    added main class (pattern chain)

---
+++ src/mainClassChainOfReq.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const r = require('./mainRequest');
+
+class Request {
+    constructor() {
+        this._url = 'https://pokeapi.co/api'
+    }
+
+    setVersion(version) {
+        this._url += '/v' + version + '/';
+        return this
+    }
+
+    get(something) {
+        this._url += something + '/';
+        return this
+    }
+
+    queryParameter(limit, offset) {
+        this._url += `?limit=${limit}` + `&offset=${offset}`
+        return this
+    }
+
+
+    request() {
+        return new Promise(resolve => {
+            r.requestMain(this._url).then(res => resolve(res))
+        })
+    }
+}
+
+module.exports = Request;
---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Request = require('../src/mainClassChainOfReq');
+
+const pokeAPI = new Request();
+//pokeAPI.setVersion(2).get('evolution-chain').request().then(res => console.log(res));
+//pokeAPI.setVersion(2).get('evolution-chain').queryParameter(20, 20).request().then(res => console.log(res));
+
+// pokeAPI.setVersion(2).get('ability').get('1').request().then(res => console.log(res));
+// pokeAPI.setVersion(2).get('ability').queryParameter(20, 20).request().then(res => console.log(res));
