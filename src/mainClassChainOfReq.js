44ad334bb1eddb4f7c2b5492fe526be726f3cb73
SashaJson <sashamiller666@gmail.com>
2021-01-27 23:01:26 +0200

    small changed in src

--- src/mainClassChainOfReq.js
+++ src/mainClassChainOfReq.js
@@ -1,32 +1,33 @@
-"use strict";
+'use strict';
 
-const r = require('./mainRequest');
+const request = require('./mainRequest');
 
 class Request {
+
     constructor() {
-        this._url = 'https://pokeapi.co/api'
+        this._url = 'https://pokeapi.co/api';
     }
 
     setVersion(version) {
         this._url += '/v' + version + '/';
-        return this
+        return this;
     }
 
     get(something) {
         this._url += something + '/';
-        return this
+        return this;
     }
 
     queryParameter(limit, offset) {
-        this._url += `?limit=${limit}` + `&offset=${offset}`
-        return this
+        this._url += `?limit=${limit}` + `&offset=${offset}`;
+        return this;
     }
 
 
     request() {
         return new Promise(resolve => {
-            r.requestMain(this._url).then(res => resolve(res))
-        })
+            request(this._url).then(res => resolve(res));
+        });
     }
 }
 
